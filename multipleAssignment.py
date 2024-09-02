bc2fbfdfb61203414b0b490bea490d72e5eaf0ef
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-09-01 23:32:00 -0500

    Python BroCode

---
+++ multipleAssignment.py
@@ -0,0 +1,19 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+# example
+#name = "Kunal"
+#age = 26
+#attractive = True
+# name, age, attractive = "Kunal", 26, True
+
+#Spongebob = 30
+#Squidward = 30
+#Sandy = 30
+#Patrick = 30
+
+Spongebob = Squidward = Sandy = Patrick = 30
+
+print(Spongebob)
+print(Sandy)
+print(Patrick)
+print(Squidward)
---
+++ variable.py
@@ -0,0 +1,30 @@
+# variable = a container for a value. Behaves as the value that it contains.
+
+# String is a series of characters
+
+#first_name = "Kunal"
+#last_name = "Shenoy"
+#full_name = first_name + " " + last_name
+
+#print(("Hello " + full_name + "!"))
+#print ("Hello " + name)
+#print(name)
+
+# Integer
+
+#age = 26
+#age += 1
+# display a variable of a different datatype, use datatype casting
+#print("Your age is " + str(age))
+#print(type(age))
+
+# Float
+
+#height = 250.5
+#rint ("Your height is " + str(height) + "cm")
+
+#print(type(height))
+
+# Boolean (True or False)
+human = True
+print(" Are you a human? " + str(human))
