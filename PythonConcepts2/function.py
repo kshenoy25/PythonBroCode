faec14e3e8bde8550eb70c8a361eecd45a533d54
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-09-10 12:54:32 -0500

    Python BroCode Update

--- PythonConcepts2/function.py
+++ PythonConcepts2/function.py
@@ -6,6 +6,31 @@ def hello(first_name, last_name, age):
     print("Have a nice day!")
 
 hello("Kunal", "Shenoy", 26)
+
+def funca (name, age = 22):
+    print(name,age)
+funca("Jack", 23)
+
+
+def funcb (input):
+    print(input + " programming language")
+funcb("python")
+
+def fun(a1 = 1, a2 = 9):
+    print(f"The numbers lies between {a1} to {a2}")
+fun(10,19)
+
+def funcx (b1, b2):
+    b1 = 2
+    b2 = 3
+    print(b1," " ,b2)
+funcx()
+
+
+def fun1(val):
+    return val + 34
+x = fun1(-34)
+print(x)
 #my_name = "Haley"
 
 #hello("Kunal")
