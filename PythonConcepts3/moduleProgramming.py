3bcff6add5e77ac40a4fc999ef5d6c8d8ab4fc13
kshenoy25 <54074703+kshenoy25@users.noreply.github.com>
2024-09-11 08:14:04 -0500

    Quiz Game updates

--- PythonConcepts3/moduleProgramming.py
+++ PythonConcepts3/moduleProgramming.py
@@ -1,6 +1,13 @@
 # module = a file containing python code. May contain functions, classes, etc
 # used with modular programming, which is to separate a program into parts
-import messages
 
-messages.hello()
-messages.bye()
+#import messages
+#import messages as msg
+#from messages import hello, bye
+
+# would not recommend for large use of code
+from messages import*
+hello()
+bye()
+
+help("modules")
