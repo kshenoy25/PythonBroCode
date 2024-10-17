66bef608f050d77038271041f00f636006035b57
KSCode <kunalshenoy25@gmail.com>
2024-10-17 15:12:01 -0500

    Update

--- PythonAggregation/main.py
+++ PythonAggregation/main.py
@@ -1,3 +1,24 @@
 # aggregation = represents a relationship where one object (the whole)
 #               contains references to one or more INDEPENDENT objects (the parts)
 
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+class Book:
+    def __init__(self, title, author):
+
+        self.title = title
+        self.author = author
+
+
+library = Library("New York Public Library")
+
+book1 = Book("Harry Potter", "J.K. Rowling")
+book2 = Book("The Hobbit", "J. R. R. Tolkein")
+book3 = Book("The Colour of Magic", "Terry Pratchet")
+
+
